f53c80470a32827e1dbd599cac5f52a0accc9917
Manoel <manoelpiovesan@gmail.com>
2023-08-30 19:56:38 -0300

    atividade 03

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# visao-computacional
+# Visão Computacional
 Repositório das atividades da disciplina de Visão Computacional na UERJ em 2023.1
--- atividade_03/script.py
+++ atividade_03/script.py
@@ -10,15 +10,13 @@ img_array = np.array(img)
 #mostrando o array de 3 dimensões
 print(img_array.shape)
 
-#   colorindo de vermelho o que for verde
+# colorindo de vermelho o que for verde
 for row in range(img_array.shape[0]):
     for col in range(img_array.shape[1]):
         pixel = img_array[row, col]
     #colorindo se for tom de verde - lembrar que eh B G R
         if pixel[1] > 80 and pixel[0] < 140 and pixel[2] < 140:
             img_array[row, col] = [0, 0, 255]
-        
-
 
 #transformando o array em imagem
 img = np.array(img_array, dtype=np.uint8)
